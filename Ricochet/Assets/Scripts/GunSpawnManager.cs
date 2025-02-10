5729324627f7d6ad6f42852bce01cd2b871c8142
Neil-McCuaig <Neil_McCuaig@bell.net>
2025-02-10 18:14:13 -0500

    Implemented cap on gun and armor spawns
    Implemented a cap on the number of armor and gun spawns that can occur. Also unsuccessfully tried to implement a check to ensure that items would not spawn on top of each other at the same spawn position. This will be implemented later.

--- Ricochet/Assets/Scripts/GunSpawnManager.cs
+++ Ricochet/Assets/Scripts/GunSpawnManager.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class GunSpawnManager : MonoBehaviour
 {
-
+    SpawnOccupied spawnOccupied;
     //A list of spawnpoints for the gun. These can me placed all over the level.
     public GameObject[] spawnPoints;
     //The gun itself. A second one of these will be needed for the armor
     public GameObject spawnableGun;
     //Same, but for the armor
     public GameObject spawnableArmor;
+
+    //Max number of guns on a stage.
+    public int gunCount;
+    //Max number of armor vests on the stage
+    public int armorCount;
 
     //Time until the gun is spawned. For the final version, this can either be used with or replaced with a check of if the players both have guns or not.
     public float timeToSpawnGun;
@@ -24,6 +29,8 @@ public class GunSpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Maybe put something to the effect of spawnOccupied = FindObjectOfType<TetrisManager>(); here? Maybe with [] in there somewhere?
+
         //timeToSpawnGun is supposed to count up from 0 until it reaches maxGunSpawnTime, then that can be used to trigger the spawn. Can / should be replaced later when we 
         //have actual guns in the character's hands.
         timeToSpawnGun = 0.0f;
@@ -32,6 +39,11 @@ public class GunSpawnManager : MonoBehaviour
         //Same as the above.
         timeToSpawnArmor = 0.0f;
         maxArmorSpawnTime = 12.0f;
+
+        //This is the default number of guns. 
+        gunCount = 0;
+        //This is the default number of armor vests
+        armorCount = 0;
     }
 
     // Update is called once per frame
@@ -44,8 +56,13 @@ public class GunSpawnManager : MonoBehaviour
         timeToSpawnArmor += Time.deltaTime;
 
         //The if statement that spawns the guns
-        if (timeToSpawnGun >= maxGunSpawnTime) 
+        if (timeToSpawnGun >= maxGunSpawnTime && gunCount < 3) 
         {
+
+
+            //Lets the code know a gun is on the stage. When a gun is picked up, this should be reduced.
+            gunCount++;
+
             //Resets the time back down to 0
             timeToSpawnGun -= maxGunSpawnTime;
 
@@ -54,8 +71,11 @@ public class GunSpawnManager : MonoBehaviour
         }
 
         //Same as above but for armor
-        if (timeToSpawnArmor >= maxArmorSpawnTime)
+        if (timeToSpawnArmor >= maxArmorSpawnTime && armorCount < 2)
         {
+            //Lets the code know a armor vest is on the stage. When a vest is picked up, this should be reduced.
+            armorCount++;
+
             //Resets the time back down to 0
             timeToSpawnArmor -= maxArmorSpawnTime;
 
