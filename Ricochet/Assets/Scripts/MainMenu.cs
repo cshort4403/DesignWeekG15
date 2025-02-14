036aba3886a13d6289871851852ff0cd57126859
cshort4403 <christopher.short1@mohawkcollege.ca>
2025-02-14 11:30:22 -0500

    Revert "Merge branch 'Random_Map'"
    This reverts commit 533171a471ee60bedc0ba039dc1dad8209a2f11e, reversing
    changes made to 8790aeeb6de62903ae817685f2b2381f676cda91.

--- Ricochet/Assets/Scripts/MainMenu.cs
+++ Ricochet/Assets/Scripts/MainMenu.cs
@@ -5,26 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
-
-    List<string> LevelNames = new List<string>();
-    bool sceneLoaded = false;
-
-	public void Start()
-	{
-        LevelNames.Add("Level1");
-		LevelNames.Add("Level2");
-		LevelNames.Add("Level3");
-	}
-
-	public void PlayGame()
+    public void PlayGame()
     {
-        if(!sceneLoaded)
-        {
-            SceneManager.LoadScene(LevelNames[Random.Range(0,3)]);
-            sceneLoaded = true;
-        }
-
-       
+        SceneManager.LoadSceneAsync("GameScene");
     }
 
     public void QuitGame()
